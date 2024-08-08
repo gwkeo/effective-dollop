c8c97e4b334b50c8c1b6a9a3e95d9f6f3036b3ad
gwkeo <raxman.i@mail.ru>
2024-08-08 22:54:33 +0300

    Различные изменения, включая перенос моделей в другую директорию

---
+++ api/DatabaseModels/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace DatabaseModels.Models;
+
+public class Category(
+    Guid id,
+    string name, 
+    string description
+    )
+{
+    public Guid Id { get; set; } = id;
+    public string Name { get; set; } = name;
+    public string Description { get; set; } = description;
+}
---
+++ api/DatabaseModels/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace DatabaseModels.Models;
+
+public class User(
+    Guid id,
+    string name,
+    string email,
+    string password
+    )
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+    
+    public string Email { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+}
